e5bb45328b4b02cfc605de1a330dece29a4453c5
Amish kumar <157312626+amish-kumar-07@users.noreply.github.com>
2025-05-24 10:14:25 +0530

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,12 +3,12 @@ import { Project } from '../types/project';
 export const projectsData: Project[] = [
   {
     id: 1,
-    title: 'Sacramental Learning',
-    description: 'A platform for spiritual learning and personal growth, offering a collection of educational resources and interactive content.',
-    image: 'https://images.pexels.com/photos/5076516/pexels-photo-5076516.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    technologies: ['React', 'Next.js', 'Tailwind CSS', 'Node.js'],
-    githubUrl: 'https://github.com/amish-kumar-07/New_Mini_Project',
-    liveUrl: 'https://sacramental-learning.netlify.app/',
+    title: 'Video Call App',
+    description: 'A simple real-time video calling app using WebRTC for peer-to-peer audio/video communication. Users can create or join a room, enabling secure 1-on-1 video calls. Built with a Node.js signaling server and Socket.IO for connection setup, the app features smooth video streaming, room management, and a clean, responsive UI.',
+    image: 'https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&w=600',
+    technologies: ['React', 'WebRTC', 'Socket', 'Node.js'],
+    githubUrl: 'https://github.com/amish-kumar-07/Video_Call-frontend',
+    liveUrl: 'https://video-call-frontend-lime.vercel.app/',
   },
   {
     id: 2,
